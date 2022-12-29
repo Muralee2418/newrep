633b469d0f69961f5de4c4beece76e7be2db95fd
Muralee2418 <muralee18@gmail.com>
2022-12-29 20:43:18 +0530

    firstcommit

---
+++ stringdemo.py
@@ -0,0 +1,8 @@
+#email=input("Enter the email")
+#email.lower().strip()
+#String is a immutable component
+name=input("Enter Your name")
+print(name[0])
+print(name.upper().strip())
+print(name.startswith('m'))
+print(name.replace('m','n'))
